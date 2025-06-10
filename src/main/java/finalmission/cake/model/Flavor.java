f4280e718bdee5e6c69a873d9ef93a7d39701699
gabean13 <choegabin171@gmail.com>
2025-06-10 17:24:43 +0900

    feat: Flavor 레포지토리 추가

--- src/main/java/finalmission/cake/model/Flavor.java
+++ src/main/java/finalmission/cake/model/Flavor.java
@@ -7,9 +7,14 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "flavor")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 public class Flavor {
 
     @Id
