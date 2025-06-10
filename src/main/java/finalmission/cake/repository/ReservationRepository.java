13d4bde9ba323ce56b21e6c3a2fbf3fcce113d3c
gabean13 <choegabin171@gmail.com>
2025-06-10 17:24:27 +0900

    feat: reservation 레포지토리 추가

---
+++ src/main/java/finalmission/cake/repository/ReservationRepository.java
@@ -0,0 +1,15 @@
+package finalmission.cake.repository;
+
+import finalmission.cake.model.Reservation;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+public interface ReservationRepository {
+
+    List<Reservation> findByCakeIdAndDate(Long cakeId, LocalDate date);
+
+    Optional<Reservation> findByCakeIdAndDateAndTimeId(Long cakeId, LocalDate date, Long timeId);
+
+    Reservation save(Reservation reservation);
+}
